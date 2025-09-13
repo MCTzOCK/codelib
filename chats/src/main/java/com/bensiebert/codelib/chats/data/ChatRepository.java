5c260be7467b883077fbff01d6e2e84e5dddb97c
Ben Siebert <hello@ben-siebert.de>
2025-09-13 18:23:22 +0200

    update

--- chats/src/main/java/com/bensiebert/codelib/chats/data/ChatRepository.java
+++ chats/src/main/java/com/bensiebert/codelib/chats/data/ChatRepository.java
@@ -1,9 +1,10 @@
 package com.bensiebert.codelib.chats.data;
 
+import com.bensiebert.codelib.auth.data.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ChatRepository extends CrudRepository<Chat, String> {
-    List<Chat> findByUserIdsContains(List<String> userIds);
+    List<Chat> findChatByUsersContaining(List<User> users);
 }
